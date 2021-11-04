d608323a31653a0fb9d30dccc1dda09df376ee9d
Gyan Prakash <gyanp008@gmail.com>
2021-11-05 05:19:07 +0530

    Secured the routes

---
+++ Backend/helperfiles/authjwt.js
@@ -0,0 +1,31 @@
+// using -> express-jwt (npm i express-jwt)
+const expressJwt = require('express-jwt');
+
+function authjwt() {
+    const secret = process.env.secret;
+    const api = process.env.api;
+    return expressJwt({ secret, algorithms: ['HS256'],isRevoked: isRevoked }).unless({
+        path: [
+
+            // public routes that don't require authentication
+            // { url: `${api}/products`, methods: ['GET','OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET','OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET','OPTIONS'] },
+            '/api/v1/users/login',
+            '/api/v1/users/register'
+            ]
+    });
+
+}
+
+async function isRevoked(req, payload, done) {
+    // revoke token if user not admin
+    if (!payload.isAdmin) {
+        return done(null, true);
+    }
+    done();
+};
+
+
+module.exports = authjwt;
+
