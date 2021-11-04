22cf4e66038dfe939acfa279566172b96564b020
Gyan Prakash <gyanp008@gmail.com>
2021-11-04 20:09:21 +0530

    created user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,8 +1,68 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    address: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    state: {
+        type: String,
+        default: ''
+    },
+    pincode: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    }
 });
+
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+});
+
+
+// userSchema.virtual('password')
+//     .set(function (password) {
+//         this._password = password;
+//         this.salt = this.makeSalt();
+//         this.hashedPassword = this.encryptPassword(password);
+//     })
+//     .get(function () {
+//         return this._password;
+//     });
 
 
 module.exports = mongoose.model('User', userSchema);
