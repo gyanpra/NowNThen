8c29b216c64048dda7c7b124688c3c73a5cebc63
Gyan Prakash <gyanp008@gmail.com>
2021-11-04 18:04:32 +0530

    product model created

--- Backend/models/product.js
+++ Backend/models/product.js
@@ -1,16 +1,71 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    description: String,
-    image: String,
-    category: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    moreDescription:{
+        type: String,
+        default: '',
+    },
+    image: {
+        type: String,
+        default: '',
+    },
+    images:[{
+        type: String,
+    }],
+    brand:{
+        type: String,
+        default: '',
+    },
+    price:{
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
     stock: {
         type: Number,
         required: true,
-    }
+        min: 0,
+        max: 100,
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    totalReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
 });
+
+productSchema.virtual('id').get(function() {
+    return this._id.toHexString();
+});
+
+productSchema.set('toJSON', {
+    virtuals: true,
+});
+
 
 
 module.exports = mongoose.model('Product', productSchema);
