ad3c42acabf08f8b779e19641e4fda582c053c47
Gyan Prakash <gyanp008@gmail.com>
2021-11-04 18:03:14 +0530

    Categories model updated

--- Backend/models/category.js
+++ Backend/models/category.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    icon: {
+        type: String,
+    },
+    color: {
+        type: String,
+    }
 
 });
+
+categorySchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+    
+    categorySchema.set('toJSON', {
+        virtuals: true,
+    });
 
 
 module.exports = mongoose.model('Category', categorySchema);
