a6cffaf6e5313dcc886c76091293da5a5568f734
Gyan Prakash <gyanp008@gmail.com>
2021-11-08 23:03:42 +0530

    order table with array of products orderitems

--- Backend/models/order.js
+++ Backend/models/order.js
@@ -1,7 +1,62 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+    }],
+    shippingAddress: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    PinCode: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'pending',
+    },
+    totalPrice: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
+    },
 
+
+
+
+});
+
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true,
 });
 
 
