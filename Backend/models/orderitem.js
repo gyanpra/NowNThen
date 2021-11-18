9569546ce74b501ed8f472060c6082ebad3ec693
Gyan Prakash <gyanp008@gmail.com>
2021-11-18 21:09:19 +0530

    routes for order added

--- Backend/models/orderitem.js
+++ Backend/models/orderitem.js
@@ -1,25 +1,17 @@
 const mongoose = require('mongoose');
 
-const orderItemSchema = new mongoose.Schema({
+const orderItemSchema = mongoose.Schema({
     quantity: {
-            ref: 'OrderItem',
+            type: Number,
             required: true
     },
     product: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product',
-        required: true
+        ref: 'Product'
     },
 
 });
 
-orderItemSchema.virtual('id').get(function () {
-    return this._id.toHexString();
-});
-
-orderItemSchema.set('toJSON', {
-    virtuals: true,
-});
 
 
 module.exports = mongoose.model('OrderItem', orderItemSchema);
